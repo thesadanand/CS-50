4372b6c71d855a4f8e1930fc7622e19db3db7571
the sadanand <saddanand@gmail.com>
2017-12-27 21:22:39 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -17,8 +17,7 @@ public class FizzBuzz {
 			System.out.println(
 					(i % 15 == 0) ? "FizzBuzz" :
 						(i % 3 == 0) ? "Fizz" :
-							(i % 5 == 0) ? "Buzz" :
-								i
+							(i % 5 == 0) ? "Buzz" : i
 					);
 		}
 	}
